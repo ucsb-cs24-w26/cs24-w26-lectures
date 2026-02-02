a070fc7df6ec5f46560893b644dd2ca1489628a3
Diba Mirza <diba@169-231-125-151.wireless.ucsb.edu>
2026-02-02 09:11:07 -0800

    lect08 stacks

---
+++ lect08/dailytemps.cpp
@@ -0,0 +1,39 @@
+//dailytemps.cpp
+//solution to leet code problem: https://leetcode.com/problems/daily-temperatures/
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+    public:
+        vector<int> dailyTemperatures_0(vector<int>& temperatures) {
+            int n = temperatures.size();
+            vector<int> result(n, 0);
+            for (int i = n - 1; i >= 0 ; i--){ 
+                for(int j = i + 1; j < temperatures.size(); j++){
+                    if(temperatures[j] > temperatures[i]){
+                        result[i] = j - i;
+                        break;
+                    }
+                }
+            } 
+            return result;     
+        }
+    
+        vector<int> dailyTemperatures(vector<int>& temperatures) {
+            int n  = temperatures.size(); 
+            vector<int> result(n, 0); 
+            stack<int> days;  
+            for (int i = n - 1; i >= 0 ; i--){ 
+                
+
+
+
+
+
+                
+            }
+            return result;   
+    
+        }
+         
+    };
