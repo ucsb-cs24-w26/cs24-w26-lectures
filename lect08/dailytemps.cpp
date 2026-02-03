19daa4259f0eeb420ea42f8c303c7c1aad445427
Diba Mirza <diba@Dibas-MacBook-Air.local>
2026-02-02 16:06:01 -0800

    lect08 -- post lecture

--- lect08/dailytemps.cpp
+++ lect08/dailytemps.cpp
@@ -12,6 +12,7 @@ class Solution {
                 for(int j = i + 1; j < temperatures.size(); j++){
                     if(temperatures[j] > temperatures[i]){
                         result[i] = j - i;
+                        break;
                     }
                 }
             } 
@@ -19,20 +20,24 @@ class Solution {
         }
     
         vector<int> dailyTemperatures(vector<int>& temperatures) {
-            int n  = temperatures.size(); 
-            vector<int> result(n, 0); 
-            stack<int> days;  
-            for (int i = n - 1; i >= 0 ; i--){ 
-                
+            int n = temperatures.size(); //O(1)
+            vector<int> result(n, 0); //O(n)
+            stack<int> s; //O(1)
+            for (int i = n - 1; i >= 0; i--){
+                while( !s.empty() && temperatures[s.top()] <= temperatures[i]){
+                    s.pop(); // How many times will we pop() over all? 
+                            // Overall: each value is popped no more than once. 
+                            // Since n values are pushed in, overall no more than n pops
+                                                                            // Overall ---> O(n)
 
-
-
-
-
-                
+                }
+                if(!s.empty()){
+                    result[i] = s.top() - i;//O(1) per iteration
+                }
+                s.push(i); // Each value is push in once per iteration O(1) ---> Total O(n) 
             }
-            return result;   
-    
+            return result;
+            
         }
          
     };
