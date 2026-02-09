1549c833a9172e810bee6cbb1186e30a276b7f41
Diba Mirza <diba@Dibas-MacBook-Air.local>
2026-02-09 08:57:27 -0800

    lect 10

---
+++ lect10/priorityqueue.cpp
@@ -0,0 +1,11 @@
+// Priority Queue Operations
+// Operations: push, pop, top
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
